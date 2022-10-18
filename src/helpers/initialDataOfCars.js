5ae4b96ae66b9e858bc6431bd6c1d4ec335e8f15
TarasPyzo <tyro.asap@gmail.com>
2022-10-18 15:48:27 +0300

    Update grouping and randomizing car login

--- src/helpers/initialDataOfCars.js
+++ src/helpers/initialDataOfCars.js
@@ -5,30 +5,28 @@ import TeamColor from './TeamColor';
 const { numberOfTeams, numberOfPlayersInTeam, step } = Game;
 
 function randomizePositionsOfCars(cars) {
-  const randomizeSorting = (a, b) => {
+  const team1 = cars.filter(car => car.teamColor === TeamColor.blue);
+  const team2 = cars.filter(car => car.teamColor === TeamColor.red);
+  const randomizeSorting = (carA, carB) => {
     const random1 = Math.round(Math.random() * 10);
     const random2 = Math.round(Math.random() * 10);
 
-    return (b + random1) - (a + random2);
+    return (carB.number + random1) - (carA.number + random2);
   };
-  const allPositions = new Array(numberOfTeams * numberOfPlayersInTeam).fill().map((item, i) => i + 1);
-  const positionsOfTeam1 = allPositions.slice(0, numberOfPlayersInTeam);
-  const positionsOfTeam2 = allPositions.slice(numberOfPlayersInTeam);
 
-  positionsOfTeam1.sort(randomizeSorting);
-  positionsOfTeam2.sort(randomizeSorting);
-  const randomPositions = positionsOfTeam1.concat(positionsOfTeam2);
+  team1.sort(randomizeSorting);
+  team2.sort(randomizeSorting);
+  const randomizedCars = team1.concat(team2);
 
-  return randomPositions.map((randomPosition, idx) => {
-    const startPosition = randomPosition;
-    const car = cars[idx];
+  return cars.map(car => {
+    const startPosition = randomizedCars.findIndex(c => c.index === car.index);
 
     return {
       ...car,
       startPosition,
       coordinate: {
         top: car.teamColor === TeamColor.blue ? 0 : numberOfPlayersInTeam * step - Game.carHeight,
-        left: car.teamColor === TeamColor.blue ? step * startPosition - step : step * (startPosition - numberOfPlayersInTeam) - step,
+        left: car.teamColor === TeamColor.blue ? step * startPosition : step * (startPosition - numberOfPlayersInTeam),
       },
     };
   });
@@ -38,8 +36,8 @@ let initialDataOfCars  = new Array(numberOfTeams * numberOfPlayersInTeam)
     .fill()
     .map((item, idx) => {
       const name = `Player${idx + 1}`;
-      const teamColor = idx < numberOfPlayersInTeam ? TeamColor.blue : TeamColor.red;
-      const direction = idx < numberOfPlayersInTeam ? CarDirection.down : CarDirection.up;
+      const teamColor = idx % 2 === 0 ? TeamColor.blue : TeamColor.red;
+      const direction = idx % 2 === 0 ? CarDirection.down : CarDirection.up;
 
       return {
         index: idx,
