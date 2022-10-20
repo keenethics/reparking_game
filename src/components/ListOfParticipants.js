b5bba884cf6ee50d3dd2485ca4537b525adc6231
TarasPyzo <tyro.asap@gmail.com>
2022-10-20 14:42:55 +0300

    Use grid to update UI of Participants section

--- src/components/ListOfParticipants.js
+++ src/components/ListOfParticipants.js
@@ -1,28 +1,41 @@
+import { Fragment } from 'react';
+
 import styles from '../styles/ListOfParticipants.module.css';
 
 function ListOfParticipants({ cars, setCars }) {
   return (
     <div className={styles.container}>
       <div className={styles.title}>Participants:</div>
-      {cars.map((car, idx) => (
-        <div
-          key={idx}
-          className={[styles.item, car.isTurn ? styles.playerTurn: ''].join(' ')}
-        >
-          {car.number}
-          {' - '}
-          <input
-            type="text"
-            className={`${car.isTurn ? styles.playerTurn: ''}`}
-            value={car.name}
-            onChange={(event => {
-              const copy = [...cars];
-              copy[car.index] = { ...copy[car.index], name: event.target.value };
-              setCars(copy);
-            })}
-          />
-        </div>
-      ))}
+      <div className={styles.grid}>
+        <div className={styles.item1}>Car</div>
+        <div className={styles.item2}>Name</div>
+        <div className={styles.item3}>Penalty</div>
+        {cars.map((car, idx) => {
+          const styleOfTurn = car.penalty ? styles.playerPenalty
+            : car.isTurn ? styles.playerTurn : '';
+
+          return (
+            <Fragment key={idx}>
+              <div className={[styles.item1, styleOfTurn].join(' ')}>
+                {car.number}
+              </div>
+              <input
+                type="text"
+                className={[styles.item2, styleOfTurn].join(' ')}
+                value={car.name}
+                onChange={(event => {
+                  const copy = [...cars];
+                  copy[car.index] = { ...copy[car.index], name: event.target.value };
+                  setCars(copy);
+                })}
+              />
+              <div className={[styles.item3, styleOfTurn].join(' ')}>
+                {car.penalty ? car.penalty : ''}
+              </div>
+            </Fragment>
+          );
+        })}
+      </div>
     </div>
   );
 }
